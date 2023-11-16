d83234014342010ee217b209a70e6d9179d970b2
Mourad Azouga <mourad-azouga@outlook.fr>
2023-11-16 21:33:14 +0100

    Added bunch of stuffies

--- opfunk3.c
+++ opfunk3.c
@@ -6,7 +6,23 @@
 */
 void modus(stack_t **stack, unsigned int line_number)
 {
+    int divoja = 0;
+   if (!stack || !*stack ||(*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    if ((*stack)->n == 0)
+    {
+        fprintf(stderr, "L%d: division by zero\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
+    (*stack) = (*stack)->next;
+    divoja =(*stack)->n % (*stack)->prev->n;
+    (*stack)->n = divoja;
+    free((*stack)->prev);
+    (*stack)->prev = NULL;
 }
 /**
  * pchar - prints char i guess
@@ -15,7 +31,19 @@ void modus(stack_t **stack, unsigned int line_number)
 */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-
+int asc;
+if (!stack || !*stack)
+{
+    fprintf(stderr,"L%d: can't pchar, value out of range", line_number);
+    exit(EXIT_FAILURE);
+}
+asc = (*stack)->n;
+if (asc < 0 || asc > 127)
+{
+    fprintf(stderr,"L%d: can't pchar, value out of range", line_number);
+    exit(EXIT_FAILURE);
+}
+printf("%c\n", asc);
 }
 
 /**
@@ -25,7 +53,15 @@ void pchar(stack_t **stack, unsigned int line_number)
 */
 void pstr(stack_t **stack, unsigned int line_number)
 {
+    stack_t *pointer = *stack;
 
+    (void)line_number;
+    while (pointer != NULL && pointer->n != 0 && pointer->n >= 0 && pointer->n <= 127)
+    {
+        putchar(pointer->n);
+        pointer = pointer->next;
+    }
+    putchar('\n');
 }
 
 /**
@@ -35,7 +71,22 @@ void pstr(stack_t **stack, unsigned int line_number)
 */
 void rotl(stack_t **stack, unsigned int line_number)
 {
+	stack_t *pointer;
+    (void) line_number;
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
 
+	pointer = *stack;
+	while (pointer->next != NULL)
+    {
+		pointer = pointer->next;
+    }
+
+	pointer->next = *stack;
+	(*stack)->prev = pointer;
+	*stack = (*stack)->next;
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL;
 }
 /**
  * rotr - does the magic stuffies but to the right
@@ -44,5 +95,20 @@ void rotl(stack_t **stack, unsigned int line_number)
 */
 void rotr(stack_t **stack, unsigned int line_number)
 {
+	stack_t *pointer;
+    (void) line_number;
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
 
+	pointer = *stack;
+	while (pointer->next != NULL)
+    {
+		pointer = pointer->next;
+    }
+
+	pointer->next = *stack;
+	pointer->next->prev = NULL;
+    pointer->prev = NULL;
+    (*stack)->prev = pointer;
+    (*stack) = pointer;
 }
