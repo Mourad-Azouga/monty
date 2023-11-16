131237891f4144d97f56ca2287d88d339f632a09
Mourad Azouga <mourad-azouga@outlook.fr>
2023-11-16 14:35:21 +0100

    m

--- listfunk.c
+++ listfunk.c
@@ -29,6 +29,7 @@ stack_t *create_new_node(int value, int sign)
 */
 void add_queue(stack_t **hamood, __attribute__((unused))unsigned  int line_n)
 {
+    stack_t *head = NULL;
     stack_t *ptr = *hamood;
 
     if (!hamood || !*hamood)
@@ -52,6 +53,7 @@ void add_queue(stack_t **hamood, __attribute__((unused))unsigned  int line_n)
 */
 void free_node(void)
 {
+    stack_t *head = NULL;
 	stack_t *ptr;
 
 	if (head == NULL)
